84c92179fbf67efa87957ade5a87a0e8ed283dae
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-05-24 23:32:03 +0530

    second add

---
+++ leapyear.java
@@ -0,0 +1,16 @@
+package day4assignment;
+import java.util.*;
+public class leapyear {
+    public static void main(String[] args) {
+
+        int year;
+        System.out.println("Enter an Year :: ");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+
+        if (year % 4 == 0)
+            System.out.println("This year is a leap year");
+        else
+            System.out.println("This year is not a leap year");
+    }
+}

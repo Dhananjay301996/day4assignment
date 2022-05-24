66535a9aeec19596e034634b96adfb52201f6610
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-05-24 23:30:07 +0530

    first add

---
+++ flipcoin.java
@@ -0,0 +1,12 @@
+package day4assignment;
+
+public class flipcoin {
+    public static void main(String[] args) {
+
+        if (Math.random() < 0.5){
+            System.out.println("Heads");
+        }else{
+            System.out.println("Tails");
+        }
+    }
+}
